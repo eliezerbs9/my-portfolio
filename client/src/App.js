67d8d3eaeb63f4b46d190c29978094fedba09a50
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-10-29 15:26:06 -0700

    css changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import WhatIdo from "./components/WhatIdo";
 import Menu from './components/Menu'
 import Projects from './components/Projects'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
@@ -11,16 +12,8 @@ function App() {
           <Menu />
       </div>
       <div className="portifolio__body">
-        <section className="about_me">
-          <h1 className="portifolio__section__header">About me</h1>
-          <p className="about_me__text">
-            I am a design oriented web developer who is passionate about
-            learning new technologies and creating beautiful web applications
-          </p>
-        </section>
-        <section className="whatIdo">
-          <WhatIdo />
-        </section>
+        <AboutMe />
+        <WhatIdo />
         <Projects />
       </div>
     </div>
---
+++ client/src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <section className="about_me">
+          <h1 className="portifolio__section__header">About me</h1>
+          <p className="about_me__text">
+            I am a design oriented web developer who is passionate about
+            learning new technologies and creating beautiful web applications
+          </p>
+        </section>
+    )
+}
+
+export default AboutMe
